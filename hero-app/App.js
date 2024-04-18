fc9eaed3a80de6a77f3cb5f0fb7d8d770622af08
Miguel Vargas <contact@miguelvargas.dev>
2024-04-18 15:31:43 -0400

    Refactor App.js and package.json to update dependencies and add new features

--- hero-app/App.js
+++ hero-app/App.js
@@ -1,59 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
-import TrackerContainer from './components/TrackerContainer/TrackerContainer';
-import AddTrackerButton from './components/AddTrackerButton';
-import Tracker from './components/Tracker/Tracker';
-import {useFonts} from 'expo-font';
-
-const backgroundImage = require('./assets/images/background-image.png');
+import React from 'react';
+import { Route, Router } from 'expo-router';
+import HomePage from './pages/HomePage';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Cinzel': require('./assets/fonts/Cinzel-Regular.ttf'),
-    'Cinzel-Bold': require('./assets/fonts/Cinzel-Bold.ttf'),
-    'Cinzel-Black': require('./assets/fonts/Cinzel-Black.ttf'),
-  });
-
   return (
-    <View style={styles.container}>
-      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
-        <View style={styles.mainContainer}>
-          <TrackerContainer>
-            <Tracker />
-          </TrackerContainer>
-          <AddTrackerButton onPress={handleAddTracker}/>
-        </View>
-      </ImageBackground>
-      <StatusBar style="auto" />
-    </View>
+    <Router>
+      <Route path="/" component={HomePage} />
+    </Router>
   );
 }
-
-function handleAddTracker() {
-  console.log('Add tracker button pressed');
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    height: '100%',
-    width: '100%',
-    fontFamily: 'Cinzel',
-  },
-  backgroundImage: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    justifyContent: 'center',
-    color: 'white',
-  },
-  mainContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    padding: 12,
-    gap: 12,
-    height: '100%',
-    width: '100%',
-  },
-});
---
+++ hero-app/pages/HomePage.js
@@ -0,0 +1,59 @@
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
+import TrackerContainer from '../components/TrackerContainer/TrackerContainer';
+import AddTrackerButton from '../components/AddTrackerButton';
+import Tracker from '../components/Tracker/Tracker';
+import {useFonts} from 'expo-font';
+
+const backgroundImage = require('./assets/images/background-image.png');
+
+export default function HomePage() {
+  const [fontsLoaded] = useFonts({
+    'Cinzel': require('./assets/fonts/Cinzel-Regular.ttf'),
+    'Cinzel-Bold': require('./assets/fonts/Cinzel-Bold.ttf'),
+    'Cinzel-Black': require('./assets/fonts/Cinzel-Black.ttf'),
+  });
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        <View style={styles.mainContainer}>
+          <TrackerContainer>
+            <Tracker />
+          </TrackerContainer>
+          <AddTrackerButton onPress={handleAddTracker}/>
+        </View>
+      </ImageBackground>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function handleAddTracker() {
+  console.log('Add tracker button pressed');
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    height: '100%',
+    width: '100%',
+    fontFamily: 'Cinzel',
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    color: 'white',
+  },
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 12,
+    gap: 12,
+    height: '100%',
+    width: '100%',
+  },
+});
